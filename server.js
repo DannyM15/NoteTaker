4f0f633f18bd978ecfe58da85cbb45361210300b
DannyM15 <merinodanny15@gmail.com>
2024-03-13 19:32:52 -0700

    notes added. ready to submit

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/apiRoutes.js')  //need routes
+const api = require('./routes/apiRoutes.js') 
 
 const PORT = process.env.PORT || 3001
-
-
 const app = express();
 
-//do we have a middleware?
+// Middleware code 
 //express.json express.urlencoded express.static 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
@@ -16,16 +14,17 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET /notes 
+// GET code for the /notes route 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//GET *
+// GET code the home page. * (wildcard) captures all other addresses  
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// 
 app.listen(PORT, () => {
     console.log('app is listening http://localhost:' + PORT)
 })
